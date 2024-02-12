25f5e5922e18480f7e68ae126f2c77f729bf35b0
Anais <zulfeqan@sheridancollege.ca>
2024-02-12 00:21:50 -0500

    Initial Commit

---
+++ WeatherUI/WeatherUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherUIApp.swift
+//  WeatherUI
+//
+//  Created by Anais Zulfequar on 2024-02-12.
+//
+
+import SwiftUI
+
+@main
+struct WeatherUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
