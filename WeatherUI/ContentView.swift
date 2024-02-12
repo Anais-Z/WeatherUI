d0e5a208927e80e3165cc31de3f2e6a2a99a4e1f
Anais <anaiszed447@gmail.com>
2024-02-12 05:09:43 -0500

    Created file for button view.

--- WeatherUI/ContentView.swift
+++ WeatherUI/ContentView.swift
@@ -126,19 +126,4 @@ struct MainWeatherStatusView: View{
 }
 
 
-struct WeatherButtonView: View{
-    
-    var title: String
-    var textColor: Color
-    var backgroundColor: Color
-    
-    var body: some View{
-        Text(title)
-            .frame(width: 280, height: 50)
-            .background(backgroundColor)
-            .foregroundColor(textColor)
-            .font(.system(size:20, weight: .bold,
-               design: .default))
-            .cornerRadius(10)
-    }
-}
+
